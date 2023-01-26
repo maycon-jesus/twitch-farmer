2c68816d3f6e29c88b5ccf78138cadba6aff93ff
Maycon Jesus <email@mayconjesus.dev>
2023-01-26 16:22:11 -0300

    feat: env prefix

--- twitch-farmer-api/src/main.ts
+++ twitch-farmer-api/src/main.ts
@@ -2,6 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+Object.keys(process.env)
+  .filter((k) => k.startsWith('TWITCH_FARMER_API_'))
+  .forEach((k) => {
+    const nKey = k.replace('TWITCH_FARMER_API_', '');
+    process.env[nKey] = k;
+  });
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
