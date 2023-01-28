3fcceca4130dd53dec7086b5919ad57a617716f8
Maycon Jesus <email@mayconjesus.dev>
2023-01-28 12:03:43 -0300

    feat: :lock: check user authorization to access data

--- twitch-farmer-api/src/guards/auth.guard.ts
+++ twitch-farmer-api/src/guards/auth.guard.ts
@@ -4,8 +4,11 @@ import {
   CanActivate,
   ExecutionContext,
   HttpException,
+  ForbiddenException,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import type Express from 'express';
+import { SessionData } from 'src/@types/sessionData';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -14,19 +17,32 @@ export class AuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request = this.getRequest(context);
 
     const authorization = request.headers.authorization;
     if (!authorization)
       throw new HttpException('Credencial de acesso inválida', 403);
 
     const payload = this.getPayload(authorization);
+
+    if (
+      request.params.userId &&
+      payload.sub.toString() != request.params.userId
+    )
+      throw new ForbiddenException();
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     request.session = payload;
 
     return true;
   }
 
-  getPayload(token: string) {
+  protected getRequest(context: ExecutionContext): Express.Request {
+    return context.switchToHttp().getRequest();
+  }
+
+  getPayload(token: string): SessionData {
     try {
       const payload = this.jwtService.verify(token);
       payload.sub = Number(payload.sub);
